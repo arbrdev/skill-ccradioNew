10fca0e49846881849cf048c02f0e9f744b497f4
arbrdev <arbr.dev@gmail.com>
2022-01-10 23:08:10 +0100

    bus2

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ from mycroft.skills.audioservice import AudioService
 
 class CCRadioNewSkill(MycroftSkill):
     def initialize(self):                   
-        self.audioservice = AudioService(self.emitter)
+        self.audioservice = AudioService(self.bus)
             
     @intent_handler("PonCCRadio.intent")
     def handle_pon_ccradio_intent(self, message):
